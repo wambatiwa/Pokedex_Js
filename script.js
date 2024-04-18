7b6c30c151e6394755930f6e188eacecdb09e389
wamba <wamb0019@algonquinlive.com>
2024-04-18 11:11:39 -0400

    catching and releasing a pokemon implementation using localstorage and array method

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const ls = JSON.parse(localStorage.getItem('caughtPoke'))
 let pokedex = []
 let offset = 0
-let indexes = []
+let caught = ls ? ls : []
+let url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`
+
 const $dialog = document.getElementById('dialog')
 const $button = document.getElementById('loadMore')
 const $closeDialog = document.getElementById('close')
-let url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`
 const $main = document.querySelector('main')
 
 function displayDetails(details) {
@@ -17,25 +19,33 @@ function displayDetails(details) {
   <p class="fs-5 h4 ">${details.height} lb</p>
   <h2 class="h2 text-center text-primary">Weight:</h2>
   <p class="fs-5 h4 ">${details.weight} ft</p>
-  <button class="btn btn-primary catch">Catch</button>
+  <button class="btn btn-primary ${caught.includes(details.name)? 'release':'catch'}" data-name ="${details.name}">${caught.includes(details.name)? 'release':'catch'}</button>
 </div>${$closeDialog.outerHTML}`
   $dialog.showModal()
   $dialog.classList.add('d-flex')
 }
+
+function displayPokemons() {
+  $main.innerHTML = pokedex.reduce((html,pokemon,index) => html + `<div id="${index}"  class="pokemon ${caught.includes(pokemon.name) ? 'caught' : ''}" data-id = "${parseUrl(pokemon.url)}">
+  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${parseUrl(pokemon.url)}.png" class="img-thumbnail" alt="${pokemon.name}">
+  <h2 class="h2 text-center" id="text">${caught.includes(pokemon.name)?"CAUGHT":pokemon.name}</h2>
+</div>`,'') 
+}
+
 async function getPokemon(url) {
     const response = await fetch(url)
     const json = await response.json()
     const pokemons = json.results
     pokedex.push(...pokemons)
     console.log(pokedex);
-    $main.innerHTML = pokedex.reduce((html,pokemon,index) => html + `<div id="${index}"  class="pokemon" data-id = "${parseUrl(pokemon.url)}">
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${parseUrl(pokemon.url)}.png" class="img-thumbnail" alt="${pokemon.name}">
-    <h2 class="h2 text-center">${pokemon.name}</h2>
-  </div>`,'') 
+    displayPokemons()
+   
 }
+
 function parseUrl (url) {
     return url.substring(url.substring(0, url.length - 2).lastIndexOf('/') + 1, url.length - 1)
 }
+
 getPokemon(url)
 
 $button.addEventListener('click', function() {
@@ -45,10 +55,10 @@ $button.addEventListener('click', function() {
 })
 
 $main.addEventListener('click', async function(e) {
+  console.log(e.target);
   const property = e.target.closest('.pokemon')
   if(property) {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${property.dataset.id}/`)
-    indexes.push(property.dataset.id)
     const result = await res.json()
     displayDetails(result)
     console.log(property);
@@ -59,15 +69,25 @@ $dialog.addEventListener('click', function(e) {
   if(e.target.classList.contains('ferme')) {
     $dialog.classList.remove('d-flex')
     $dialog.close()
+    
   }
-  // if(e.target.classList.contains('catch')) {
-  //   $dialog.classList.remove('d-flex')
-  //   $dialog.close()
-  //   console.log(property.id);
-  // }
+  if(e.target.classList.contains('catch')) {
+    $dialog.classList.remove('d-flex')
+    $dialog.close()
+    caught.push(e.target.dataset.name)
+    localStorage.setItem('caughtPoke',JSON.stringify(caught))
+    displayPokemons()
+  }
+  if(e.target.classList.contains('release')) {
+    $dialog.classList.remove('d-flex')
+    $dialog.close()
+    caught = caught.filter(poke => poke !== e.target.dataset.name)
+    localStorage.setItem('caughtPoke',JSON.stringify(caught))
+    // const index = caught.indexOf(e.target.dataset.name)
+    // caught.splice(index,1)
+    displayPokemons()
+  }
 })
-
-
 
 
 
